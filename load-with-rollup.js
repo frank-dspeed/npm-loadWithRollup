fb1341f0b5c4fc2f24d493f759d4ee58d9b6f6e3
Frank Lemanschik <frank@lemanschik.com>
2022-09-09 12:53:44 +0200

    Create load-with-rollup.js

---
+++ load-with-rollup.js
@@ -0,0 +1,6 @@
+/**
+ * Implements interpretation of the npm:protocol in source files and node: as also adds cmake: gcc: cargo: 
+ * protocols like vcpkg: can be impl on top of cmake or gcc msys2 support is also planned.
+ * This is pre work for the stealify compiler @stealify/compiler we use rollup to parse @stealify/script
+ * 
+ */
